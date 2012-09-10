0bada454c7b05a657ca0a0e1e52a12dee5cb0857
Patrik.L. <itutvecklare@gmail.com>
2012-09-10 17:47:01 +0200

    Test commit from  W shell

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,4 +1,5 @@
 <div class="container">
 	<h2>Container - Main content</h2>
 	<p>Some content goes here...</p>
+	<p>Enter your replacement text here...Lorem ipsum dolor sit amet, cum mollis timeam lucilius ex, ea usu facete appellantur, id mel ubique inermis salutatus. Tation noster cu pri, ad per unum tractatos. In sea prima epicuri, timeam insolens eu vis. Eos cu autem copiosae dignissim. Vel vocent epicuri menandri in, fabellas forensibus ex qui, putant appetere dissentiunt sed at. Modus doming eu sed, melius habemus mea at.</p>
 </div> <!-- end container -->
